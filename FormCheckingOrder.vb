8c188ed204f4a8dd09d4ab5d44a751ad2005866b
Agahari <agahariiswarajati@gmail.com>
2020-12-02 15:46:03 +0700

    Shopee masih belum bisa import

--- FormCheckingOrder.vb
+++ FormCheckingOrder.vb
@@ -1,5 +1,6 @@
 ﻿Imports System.Data.SqlClient
 Imports System.Data.OleDb
+Imports Microsoft.Office.Interop
 Public Class FormCheckingOrder
 	Private Sub ButtonExit_Click(sender As Object, e As EventArgs) Handles ButtonExit.Click
 		Me.Close()
@@ -60,6 +61,11 @@ Public Class FormCheckingOrder
 	Private Sub ButtonImport_Click(sender As Object, e As EventArgs) Handles ButtonImport.Click
 
 		If ComboBoxPilihMP.Text = "TokoPedia" Then
+			Dim Loc As String
+			Loc = "PP343"
+
+			Dim Tanggal As Date
+			Tanggal = Today
 			Try
 				Dim CariFile As New OpenFileDialog
 				With CariFile
@@ -86,20 +92,77 @@ Public Class FormCheckingOrder
 					For Each DrwEs In DsEx.Tables(0).Rows
 
 						Dim CmdEs As New SqlCommand("INSERT INTO MP_CheckingOrder (MP,Order_Id,Invoice,Resi,Isbn,Qty,Harga,Ongkir,Location,Proses_Status,Tanggal_Proses) VALUES ('" & ComboBoxPilihMP.SelectedItem & "', '" & DrwEs(1).ToString & "','" & DrwEs(2).ToString & "',
-												'" & DrwEs(23).ToString & "','" & DrwEs(8).ToString & "'," & DrwEs(7) & "," & DrwEs(10) & "," & DrwEs(19) & ",'PP343','1','' ) ", ConnEs)
+												'" & DrwEs(23).ToString & "','" & DrwEs(8).ToString & "'," & DrwEs(7) & "," & DrwEs(10) & "," & DrwEs(19) & ",'" & Loc & "','1','" & Tanggal & "' ) ", ConnEs)
 						DrEs = CmdEs.ExecuteReader
 						DrEs.Close()
 					Next
 
 					ConnEs.Close()
 					MsgBox("Wis Rampung")
+					DGV_MPCheckingOrder.Update()
+
+
+
+
+				End If
+			Catch ex As Exception
+
+			End Try
+		ElseIf ComboBoxPilihMP.Text = "Shopee" Then
+			Dim Loc As String
+			Loc = "PP342"
+
+			Dim Tanggal As Date
+			Tanggal = Today
+
+			Try
+				Dim CariFile As New OpenFileDialog
+				With CariFile
+					.Filter = " Shopee (*.xls)|*.xls|All Files (*.*)|*.* "
+					.FilterIndex = 1
+					.Title = "Pilih File"
+				End With
+
+				If CariFile.ShowDialog() = Windows.Forms.DialogResult.OK Then
+					Dim NamaFile = CariFile.FileName
+					Dim ConnEx As New OleDbConnection("Provider = Microsoft.ACE.OleDb.12.0 ; Data Source = '" & NamaFile & "'; Extended Properties = "" Excel 12.0 xml;IMEX = 1;  HDR = YES"" ")
+					ConnEx.Open()
+
+					Dim DaEx As New OleDbDataAdapter("SELECT * FROM [Sheets1$A1:AS2]", ConnEx)
+					DaEx.TableMappings.Add("'" & NamaFile & "'", "MP_CheckingOrder")
+					Dim DsEx As New DataSet
+					DaEx.Fill(DsEx)
+					ConnEx.Close()
+
+					Dim ConnEs As New SqlConnection("Data Source = AERYICE-PC666\SQLEXPRESS ; Initial Catalog = INV ; Integrated Security = TRUE")
+					ConnEs.Open()
+					Dim DrwEs As DataRow
+					Dim DrEs As SqlDataReader
+
+					For Each DrwEs In DsEx.Tables(0).Rows
+						Dim CmdEs As New SqlCommand("INSERT INTO MP_CheckingOrder (MP,Invoice,Resi,Isbn,Qty,Harga,Ongkir,Location,Proses_Status,Tanggal_Proses) 
+													VALUES ('" & ComboBoxPilihMP.SelectedItem & "','" & DrwEs(0).ToString & "','" & DrwEs(4).ToString & "','" & DrwEs(11).ToString & "',
+													" & DrwEs(17) & "," & DrwEs(15) & "," & DrwEs(33) & ",'" & Loc & "','1','" & Tanggal & "')", ConnEs)
+						DrEs = CmdEs.ExecuteReader
+						DrEs.Close()
+
+					Next
+					ConnEs.Close()
+					
+			
+					MsgBox("RAmpung")
+
+
+
+
 
 				End If
 			Catch ex As Exception
 
 			End Try
 		Else
-			MsgBox("Sik Rung Rampung")
+			MsgBox("Pilih sik Tokone")
+
 		End If
 
 	End Sub

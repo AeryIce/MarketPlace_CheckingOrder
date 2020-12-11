e14a92cdf0404461fbf0a6b2f5c9e5446aa00d29
Agahari <agahariiswarajati@gmail.com>
2020-12-11 15:08:34 +0700

    Shopee need some improvement

--- FormCheckingOrder.vb
+++ FormCheckingOrder.vb
@@ -92,8 +92,8 @@ Public Class FormCheckingOrder
 
 					For Each DrwEs In DsEx.Tables(0).Rows
 
-						CmdEs = New SqlCommand("INSERT INTO MP_CheckingOrder (MP,Order_Id,Tokped_ProductID,Invoice,Resi,Isbn,Qty,Harga,Ongkir,Location,Tanggal_Proses) VALUES ('" & ComboBoxPilihMP.SelectedItem & "', '" & DrwEs(1).ToString & "','" & DrwEs(5).ToString & "','" & DrwEs(2).ToString & "',
-												'" & DrwEs(23).ToString & "','" & DrwEs(8).ToString & "'," & DrwEs(7) & "," & DrwEs(10) & "," & DrwEs(19) & ",'" & Loc & "','" & Tanggal & "' ) ", ConnEs)
+						CmdEs = New SqlCommand("INSERT INTO MP_CheckingOrder (MP,Order_Id,Tokped_ProductID,Invoice,Resi,Isbn,Qty,Harga,Ongkir,Location,Proses_Status,Tanggal_Proses) VALUES ('" & ComboBoxPilihMP.SelectedItem & "', '" & DrwEs(1).ToString & "','" & DrwEs(5).ToString & "','" & DrwEs(2).ToString & "',
+												'" & DrwEs(23).ToString & "','" & DrwEs(8).ToString & "'," & DrwEs(7) & "," & DrwEs(10) & "," & DrwEs(19) & ",'" & Loc & "','','" & Tanggal & "' ) ", ConnEs)
 						DrEs = CmdEs.ExecuteReader
 						DrEs.Close()
 
@@ -161,10 +161,10 @@ Public Class FormCheckingOrder
 
 				If CariFile.ShowDialog() = Windows.Forms.DialogResult.OK Then
 					Dim NamaFile = CariFile.FileName
-					Dim ConnEx As New OleDbConnection("Provider = Microsoft.ACE.OleDb.12.0 ; Data Source = '" & NamaFile & "'; Extended Properties = "" Excel 12.0 xml;IMEX = 1;  HDR = YES"" ")
+					Dim ConnEx As New OleDbConnection(" Provider = Microsoft.ACE.OLEDB.12.0; Data Source = '" & NamaFile & "';Extended Properties = ""Excel 12.0 xml;IMEX=1;HDR = Yes"" ")
 					ConnEx.Open()
 
-					Dim DaEx As New OleDbDataAdapter("SELECT * FROM [Sheets1$A1:AS2]", ConnEx)
+					Dim DaEx As New OleDbDataAdapter("SELECT * FROM [orders$A1:AS5000]", ConnEx)
 					DaEx.TableMappings.Add("'" & NamaFile & "'", "MP_CheckingOrder")
 					Dim DsEx As New DataSet
 					DaEx.Fill(DsEx)
@@ -172,30 +172,66 @@ Public Class FormCheckingOrder
 
 					Dim ConnEs As New SqlConnection("Data Source = AERYICE-PC666\SQLEXPRESS ; Initial Catalog = INV ; Integrated Security = TRUE")
 					ConnEs.Open()
-					Dim DrwEs As DataRow
-					Dim DrEs As SqlDataReader
+					Dim DrwEs1 As DataRow
+					Dim DrEs1 As SqlDataReader
 
-					Dim HargaAwal As String = DrwEs(15)
-					Dim harga As String = HargaAwal.Replace("Rp", String.Empty)
-					HargaAwal = harga.Replace(".", String.Empty)
+					For Each DrwEs1 In DsEx.Tables(0).Rows
 
-					Dim Ongkir As String = DrwEs(33)
-					Dim HargaOngkir As String = Ongkir.Replace("Rp", String.Empty)
-					HargaOngkir = HargaOngkir.Replace(".", String.Empty)
+						Dim HargaAwal As String = DrwEs1(15)
+						Dim harga As String = HargaAwal.Replace("Rp", String.Empty)
+						HargaAwal = harga.Replace(".", String.Empty)
+						Dim HargaFinal As Integer = Integer.Parse(HargaAwal)
 
-					For Each DrwEs In DsEx.Tables(0).Rows
-						Dim CmdEs As New SqlCommand("INSERT INTO MP_CheckingOrder (MP,Invoice,Resi,Isbn,Qty,Harga,Ongkir,Location,Proses_Status,Tanggal_Proses) 
-													VALUES ('" & ComboBoxPilihMP.SelectedItem & "','" & DrwEs(0).ToString & "','" & DrwEs(4).ToString & "','" & DrwEs(11).ToString & "',
-													" & DrwEs(17) & "," & DrwEs(15) & "," & DrwEs(33) & ",'" & Loc & "','1','" & Tanggal & "')", ConnEs)
-						DrEs = CmdEs.ExecuteReader
-						DrEs.Close()
+						Dim Ongkir As String = DrwEs1(33)
+						Dim HargaOngkir As String = Ongkir.Replace("Rp", String.Empty)
+						HargaOngkir = HargaOngkir.Replace(".", String.Empty)
+						Dim FinalOngkir As Integer = Integer.Parse(HargaOngkir)
+
+						Dim CmdEs As New SqlCommand("INSERT INTO MP_CheckingOrder (MP,Order_Id,Tokped_ProductID,Invoice,Resi,Isbn,Qty,Harga,Ongkir,Location,Proses_Status,Tanggal_Proses) 
+													VALUES ('" & ComboBoxPilihMP.SelectedItem & "','" & DrwEs1(0).ToString & "','" & DrwEs1(11).ToString & "','" & DrwEs1(0).ToString & "','" & DrwEs1(4).ToString & "','" & DrwEs1(11).ToString & "',
+													'" & DrwEs1(17) & "','" & HargaFinal & "','" & FinalOngkir & "','" & Loc & "','','" & Tanggal & "')", ConnEs)
+						DrEs1 = CmdEs.ExecuteReader
+						DrEs1.Close()
 
 					Next
-					ConnEs.Close()
-					
-			
-					MsgBox("RAmpung")
 
+					Dim ConnEsDup As SqlConnection
+					ConnEsDup = New SqlConnection("Data Source = AERYICE-PC666\SQLEXPRESS; Initial Catalog = INV ; Integrated Security = True")
+					ConnEsDup.Open()
+					Dim CmdEsDup As SqlCommand
+					CmdEsDup = New SqlCommand("
+					'WITH DUP AS( 
+					'SELECT 
+					'MP,
+					'Order_ID,
+					'Tokped_ProductId,
+					'Invoice,
+					'Proses_Status,
+					'ROW_NUMBER() OVER(
+					'	PARTITION BY
+					'	MP,
+					'	Order_ID,
+					'	Tokped_ProductId,
+					'	Invoice,
+					'	Proses_Status
+					'ORDER BY
+					'	MP,
+					'	Order_ID,
+					'	Tokped_ProductId,
+					'	Invoice,
+					'	Proses_Status
+					')ROW_NUM 
+					'FROM MP_CheckingOrder)
+					'DELETE FROM DUP
+					'WHERE ROW_NUM > 1 AND Proses_Status IS NULL", ConnEsDup)
+
+					'Dim DrEsDUp As SqlDataReader
+					'DrEsDUp = CmdEsDup.ExecuteReader
+					'DrEsDUp.Close()
+
+					MsgBox("Import Shopee Success!!")
+					ConnEs.Close()
+					ConnEsDup.Close()
 
 
 

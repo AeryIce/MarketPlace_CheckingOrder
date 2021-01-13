03ec5b6cb93bf8419bedb2991f1c3836c1d4dbb3
Agahari <agahariiswarajati@gmail.com>
2021-01-13 16:18:34 +0700

    yosshhh.. auto centang done... lanjut cek button proses

--- FormValidasi.vb
+++ FormValidasi.vb
@@ -13,7 +13,7 @@ Public Class FormValidasi
         chk.Name = "chk"
         DGVInvoice.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells
         DGVInvoice.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells
-        BtProses.Enabled = False
+        'BtProses.Enabled = False
     End Sub
     Private Sub DGVInvoice_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DGVInvoice.CellContentClick
         Dim senderGrid As DataGridView = sender
@@ -27,34 +27,38 @@ Public Class FormValidasi
         End If
     End Sub
     Private Sub ButtonProses_Click(sender As Object, e As EventArgs) Handles BtProses.Click
-        Call Konek()
+
         Dim idxRow As DataGridViewCheckBoxCell = DGVInvoice.Rows(0).Cells(5)
-        Dim intRow As Integer = idxRow.RowIndex
-        Try
-            Dim CheckedRows =
-                (
-                    From Rows In DGVInvoice.Rows.Cast(Of DataGridViewRow)()
-                    Where CBool(Rows.Cells(5).Value) = True
-                ).ToList
+            Dim intRow As Integer = idxRow.RowIndex
+            Try
+                Dim CheckedRows =
+                    (
+                        From Rows In DGVInvoice.Rows.Cast(Of DataGridViewRow)()
+                        Where CBool(Rows.Cells(5).Value) = True
+                    ).ToList
 
-            If CheckedRows.Count = 0 Then
-                MsgBox("Checklist Data yang akan diupdate !!!")
-                Exit Sub
-            Else
-                For Each row As DataGridViewRow In CheckedRows
-                    Cmd = New SqlCommand("UPDATE MP_CheckingOrder set Proses_status=1 WHERE order_id in('" & row.Cells(1).Value & "') and isbn in('" & row.Cells(3).Value & "')", ConnectDb)
-                    Dr = Cmd.ExecuteReader
-                    Dr.Close()
-                Next
-                MsgBox("Update Succes !!!")
-                Me.Close()
-                FormCheckingOrder.Show()
-                FormCheckingOrder.TextBoxScanIsbn.Clear()
-                FormCheckingOrder.DGV_MPCheckingOrder.Columns.Clear()
-            End If
-        Catch ex As Exception
+                If CheckedRows.Count = 0 Then
+                    MsgBox("Checklist Data yang akan diupdate !!!")
+                    Exit Sub
+                Else
+                    For Each row As DataGridViewRow In CheckedRows
+                        Cmd = New SqlCommand("UPDATE MP_CheckingOrder set Proses_status=1 WHERE order_id in('" & row.Cells(1).Value & "') and isbn in('" & row.Cells(3).Value & "')", ConnectDb)
+                        Dr = Cmd.ExecuteReader
+                        Dr.Close()
+                    Next
+                    MsgBox("Update Succes !!!")
+                    Me.Close()
+                    FormCheckingOrder.Show()
+                    FormCheckingOrder.TextBoxScanIsbn.Clear()
+                    FormCheckingOrder.DGV_MPCheckingOrder.Columns.Clear()
+                End If
+            Catch ex As Exception
 
-        End Try
+            End Try
+
+
+
+
     End Sub
 
     Private Sub TextBoxvalidasiisbn_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBoxvalidasiisbn.KeyPress
@@ -65,29 +69,48 @@ Public Class FormValidasi
     End Sub
 
     Private Sub BtCari_Click(sender As Object, e As EventArgs) Handles BtCari.Click
-        Try
-            Dim Hasil As Integer = 0
 
-            For CariBaris As Integer = 0 To DGVInvoice.RowCount - 1
-                For CariKolom As Integer = 0 To DGVInvoice.ColumnCount - 1
-                    If DGVInvoice.Rows(CariBaris).Cells(CariKolom).Value.ToString = TextBoxvalidasiisbn.Text Then
-                        MsgBox("Item found")
-                        DGVInvoice.Rows(DGVInvoice.CurrentCell.RowIndex).Cells(5).Value = True
+        Dim Hasil As Integer = 0
+        'Dim chk As New DataGridViewCheckBoxColumn()
+        'DGVInvoice.Columns.Add(chk)
+        'chk.HeaderText = "Check/Uncheck"
+        'chk.Name = "chk"
+        'DGVInvoice.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells
+        'DGVInvoice.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells
+        'BtProses.Enabled = False
 
-                        Hasil = 1
+        For CariBaris As Integer = 0 To DGVInvoice.RowCount - 1
+
+            If DGVInvoice.Rows(CariBaris).Cells(3).Value = TextBoxvalidasiisbn.Text Then
+
+                Try
+
+                    Hasil = 1
+                Catch ex As Exception
+
+                    MessageBox.Show(ex.Message)
+                    If ex.InnerException IsNot Nothing Then
+                        MsgBox(ex.InnerException)
                     End If
-                Next
-            Next
-            If Hasil = 0 Then
-                MsgBox("Item not found")
+                End Try
+
+                DGVInvoice.Rows(CariBaris).Cells(5).Value = True
+                BtProses.Enabled = True
             End If
 
 
-        Catch ex As Exception
-            MessageBox.Show(ex.Message)
 
 
-        End Try
+
+
+
+
+        Next
+        If Hasil = 0 Then
+            MsgBox("Ora ONO!!")
+        End If
+
+
     End Sub
 
 
